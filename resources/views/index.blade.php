27c36e9c64877a3451f68dea8badea4c4979f4e3
sadegh <mirsadeghporbehnam831@gmail.com>
2020-08-18 22:35:34 +0430

    createProject

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('content')
+    
+
+   @include('partials.preloader')
+   @include('partials.menu')
+   @include('partials.header')
+   @include('partials.features')
+   @include('partials.services')
+   @include('partials.spical_features')
+   @include('partials.prices')
+   @include('partials.team')
+   @include('partials.testimonial')
+   @include('partials.faq')
+   @include('partials.clients')
+   @include('partials.blogs')
+   @include('partials.footer')
+
+
+@endsection
+
+ 
+
+
+
