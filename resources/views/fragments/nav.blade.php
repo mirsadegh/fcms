55fef18ccb66ab8514f83a5e33f0ee16d9a8020f
sadegh <mirsadeghporbehnam831@gmail.com>
2020-08-20 16:55:29 +0430

    ui improvement header edit form

--- resources/views/fragments/nav.blade.php
+++ resources/views/fragments/nav.blade.php
@@ -3,7 +3,8 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -23,15 +24,22 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right text-right" aria-labelledby="navbarDropdown">
+
+                            <a class="dropdown-item" href="{{ url('home') }}">
+                                <i class="ti-dashboard ml-1"></i>
+                                داشبورد
+                            </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                               خروج
+                               onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
+                                <i class="ti-lock ml-1"></i>
+                                خروج
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
