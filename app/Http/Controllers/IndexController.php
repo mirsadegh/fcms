434451daf20b682427098b00ad4b3c944a792390
sadegh <mirsadeghporbehnam831@gmail.com>
2020-08-23 18:27:21 +0430

    send Ajax Request for Message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Footer;
 use App\Header;
+use App\Message;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -13,6 +14,23 @@ class IndexController extends Controller
         $header = Header::first() ?? new Header;
         $footer = Footer::first() ?? new Footer;
         return view('index',compact('header','footer'));
+
+    }
+
+    public function store_message(Request $request)
+    {
+
+        $data = $request->validate([
+            'name'    => 'required|string|max:191',
+            'email'   => 'required|string|max:191',
+            'subject' => 'nullable|string|max:191',
+            'body'    => 'required|string|max:1000',
+        ]);
+
+        Message::create($data);
+        $result ['success'] = true;
+        $result['message'] = 'پیام شما با موفقیت در سیستم ثبت گردید.';
+        return $result;
 
     }
 }
