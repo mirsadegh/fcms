eb005e45b17b91a47a023c98db93258f8caf26b0
sadegh <mirsadeghporbehnam831@gmail.com>
2020-09-01 21:02:05 +0430

    Dynamic Cards and Prices

--- resources/views/partials/prices.blade.php
+++ resources/views/partials/prices.blade.php
@@ -4,60 +4,31 @@
             <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
                 <div class="page-title">
                     <h2>Afortable Price</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit voluptates, temporibus at, facere harum fugiat!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit voluptates,  facere harum fugiat!</p>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12 col-sm-4">
+
+            @foreach($section->contents as $content)
+            <div class="col-md-{{ $content->cols }}">
                 <div class="price-table">
-                    <h3 class="text-uppercase price-title">Basic</h3>
+                    @if($content->icon)
+                    <span class="price-info"><i class="fa fa-{{ $content->icon }}"></i></span>
+                    @endif
+                    <h3 class="text-uppercase price-title">{{ $content->title }}</h3>
                     <hr>
                     <ul class="list-unstyled">
-                        <li><strong class="amount">$ <span class="big">20</span></strong>/Month</li>
-                        <li>100 MB Disk Space</li>
-                        <li>2 Subdomains</li>
-                        <li>5 Email Accounts</li>
-                        <li>Webmail Support</li>
-                        <li>Customer Support 24/7</li>
+                       @foreach(explode("\n",$content->description) as $value)
+                        <li>{{ $value }}</li>
+                       @endforeach
                     </ul>
                     <hr>
-                    <a href="#" class="button">Purchase</a>
+                    <a href="{{ $content->link_href }}" class="button">{{ $content->link_name }}</a>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-4">
-                <div class="price-table active">
-                    <span class="price-info"><span class="ti-crown"></span></span>
-                    <h3 class="text-uppercase price-title">STABDARD</h3>
-                    <hr>
-                    <ul class="list-unstyled">
-                        <li><strong class="amount">$ <span class="big">39</span></strong>/Month</li>
-                        <li>100 MB Disk Space</li>
-                        <li>2 Subdomains</li>
-                        <li>5 Email Accounts</li>
-                        <li>Webmail Support</li>
-                        <li>Customer Support 24/7</li>
-                    </ul>
-                    <hr>
-                    <a href="#" class="button">Purchase</a>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-4">
-                <div class="price-table">
-                    <h3 class="text-uppercase price-title">UNLIMITED</h3>
-                    <hr>
-                    <ul class="list-unstyled">
-                        <li><strong class="amount">$ <span class="big">59</span></strong>/Month</li>
-                        <li>100 MB Disk Space</li>
-                        <li>2 Subdomains</li>
-                        <li>5 Email Accounts</li>
-                        <li>Webmail Support</li>
-                        <li>Customer Support 24/7</li>
-                    </ul>
-                    <hr>
-                    <a href="#" class="button">Purchase</a>
-                </div>
-            </div>
+            @endforeach
+
         </div>
     </div>
 </section>
